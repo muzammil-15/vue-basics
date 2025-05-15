f58c335fac61b69b7c4426dbf13f80bbb5f02d17
muzammil-15 <muzammilcoder15@gmail.com>
2025-05-15 21:11:09 +0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+var name = "Muzammil"
+
+var links = ["home", "about", "contact", "profile"]
+
+
+</script>
+
+<template>
+    <div class="w-full h-screen flex flex-col gap-1 justify-center items-center">
+
+        <h1>Welcome back {{ name }}</h1>
+        <p v-if="age < 18">he cannot vote</p>
+        <p v-if="age >= 18">he can vote</p>
+
+        <p v-for="(x, i) in links">{{ i + ":" + x }}</p>
+
+
+
+
+
+
+    </div>
+
+</template>
---
+++ src/components/button.vue
@@ -0,0 +1,9 @@
+<template>
+
+    <a href="" class="bg-black text-white py-2 px-6 rounded hover:bg-gray-700">
+
+        <slot></slot>
+    </a>
+
+
+</template>
